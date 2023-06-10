26dc7585d26629bcf0f8db5d7a1b3eab916d8ea7
Singh <sabhisheksignh343104@gmail.com>
2023-06-10 20:01:40 +0530

    Added comments

--- decimal_to_binary.c
+++ decimal_to_binary.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
+// Defining a function that takes two numbers and returns quotient
 int quotient(int dividendo, int divisor){
     int quotient;
     quotient = dividendo/divisor;
     return quotient;
 }
 
+// Defining a function that takes an integer number, then it prints after converting this integer into binary form
 int decimal_to_binary(int num){
     int arr[100], remainder, i=0;
     while (num)
@@ -24,9 +26,14 @@ int decimal_to_binary(int num){
 
 int main()
 {
+    // Declaring variable
     int num;
+
+    // Taking number as input from user
     printf("Enter the number to convert in binary: ");
     scanf("%d", &num);
+
+    // Calling decimal_to_binary function
     decimal_to_binary(num);
 
     return 0;
